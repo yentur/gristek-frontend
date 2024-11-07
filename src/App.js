6566188016e3889b16d45ddd16b8807af9a0e9bb
atş <214210081@ogrenci.hitit.edu.tr>
2024-11-07 21:09:51 +0300

    Fix tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './main.css';
 import Homepage from './components/homepage';
 import ContactPage from './components/ContactPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
