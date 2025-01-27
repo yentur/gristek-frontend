bb6b19c0c1eb361db6196432c7438969dd901c50
atss0 <ahmettayyip56@gmail.com>
2025-01-27 20:26:25 +0300

    Update OurReferences

--- src/components/Home/OurReferences.jsx
+++ src/components/Home/OurReferences.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-// Carousel bileşenini ve stillerini import ediyoruz
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-// Örnek referans verisi:
 const references = [
   {
     name: "Çorum Belediyesi",
@@ -23,39 +21,35 @@ const references = [
       "https://yt3.googleusercontent.com/Oay2Dzbg23llfPESqmIZft0gOrGzwAMuSQAcrng5NzV_Zv1diaBXfkRkgW-_Cy3blSvSEdok=s900-c-k-c0x00ffffff-no-rj",
     url: "https://www.istac.istanbul",
   },
-  // Daha fazla logo ekleyebilirsiniz...
 ];
 
 const OurReferences = () => {
   const extendedReferences = [...references, ...references, ...references];
-  
+
   return (
     <section className="md:py-20 py-10 relative border-t-2 border-b-2 border-gray-200">
       <div className="mx-auto text-center">
-      <h2 className="text-4xl lg:text-5xl font-bold mb-4">
-            Referanslarımız
-          </h2>
+        <h2 className="text-4xl lg:text-5xl font-bold mb-4">Referanslarımız</h2>
 
-        {/* Carousel ayarlarımız */}
         <Carousel
-          showThumbs={false}       // Alttaki küçük önizlemeleri gizler
-          showStatus={false}       // Üstteki sayfa numarasını gizler
-          infiniteLoop={true}      // Sonsuz döngü
-          autoPlay={true}          // Otomatik geçiş
-          interval={3000}          // Her 3 saniyede bir geçiş
-          transitionTime={600}     // Geçiş süresi (ms cinsinden)
-          stopOnHover={true}       // Fareyle üzerine gelince dur
-          centerMode={true}        // Orta odaklı görünüm
-          centerSlidePercentage={33} // Ekranın kaçta kaçını kaplasın (%)
-          emulateTouch={true}      // Mobilde dokunmatik kaydırma
-          swipeable={false}        // Fare/Parmakla sürükleme
-          showIndicators={false}   // Nokta göstergelerini gizler
+          showThumbs={false}
+          showStatus={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={3000}
+          transitionTime={600}
+          stopOnHover={true}
+          centerMode={true}
+          centerSlidePercentage={33}
+          emulateTouch={true}
+          swipeable={false}
+          showIndicators={false}
         >
           {extendedReferences.map((ref, index) => (
             <div key={index} className="px-2">
               <div
                 className="bg-white flex items-center justify-center"
-                style={{ height: 200 }} // Logo kartınızın yüksekliğini ayarlayın
+                style={{ height: 200 }}
               >
                 <img
                   src={ref.avatar}
